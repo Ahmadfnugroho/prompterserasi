6b762f01ada9bd15865453b86694471dfb5fe26e
Ahmad Fauzi Nugroho <ahmadkenug@gmail.com>
2025-04-14 00:54:53 +0700

    perubahan 1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/prompterserasi/", // Sesuaikan dengan nama repo
+  base: "/",
   plugins: [react()],
 });
